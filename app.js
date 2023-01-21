c7fa04b4efeaea11c9d8fc686ef318724d1c9e40
Deepesh <91532309+deepesh06-95@users.noreply.github.com>
2023-01-22 01:18:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,3 +8,68 @@ const tiles = L.tileLayer(titleUrl, { attribution });
 
 tiles.addTo(map);
 
+
+const generateList = () => {
+    const ul = document.querySelector('.list')
+
+    storeList.forEach((shop) => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        const a = document.createElement('a');
+        const p = document.createElement('p');
+        div.classList.add('shop-item');
+        a.innerText = shop.properties.name
+        a.href = '#';
+        a.addEventListener('click', () => {
+            flyToStore(shop)
+        })
+        p.innerText = shop.properties.address;
+        div.appendChild(a)
+        div.appendChild(p)
+        li.appendChild(div)
+        ul.appendChild(li)
+    })
+}
+generateList()
+
+const popUpContent = (data) => {
+    return `
+<div>
+<h4>${data.properties.name}</h4>
+<p>${data.properties.address}</p>
+<div class='phoneNumber'><a href='tel:${data.properties.phone}'>${data.properties.phone}</a></div>
+</div>
+`
+}
+const onEachFeature = (feature, layer) => {
+    layer.bindPopup(popUpContent(feature), {
+        closeButton: false,
+        offset: L.point(0, -7)
+    })
+}
+const myIcon = L.icon({
+    iconUrl: 'food.png',
+    iconSize: [30, 30],
+})
+const shopsLayer = L.geoJSON(storeList, {
+    onEachFeature: onEachFeature,
+    pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, {
+            icon: myIcon
+        })
+    }
+});
+
+shopsLayer.addTo(map)
+
+const flyToStore = (store) => {
+    const lat = store.geometry.coordinates[1];
+    const lng = store.geometry.coordinates[0];
+
+    map.flyTo([lat, lng], 14, {
+        duration: 3
+    });
+    setTimeout(() => {
+        L.popup({ closeButton: false, offset: L.point(0, -7) }).setLatLng([lat, lng]).setContent(popUpContent(store)).openOn(map)
+    }, 3000)
+}
--- store.js
+++ store.js
@@ -1,3 +1,4 @@
+// Geojson
 const storeList = [
     {
         "type": "Feature",
@@ -6,7 +7,7 @@ const storeList = [
             "coordinates": [72.82952788802635, 18.920675417289807]
         },
         "properties": {
-            "name": "Pizza outlet 1",
+            "name": "Deepesh's Kitchen 1",
             "address": "Ground Floor, Strand Coffee House, PJ Ramchandani Marg, Apollo Bandar, Colaba, Mumbai, Maharashtra 400005, India",
             "phone": "23 2323 2323"
         }
@@ -18,7 +19,7 @@ const storeList = [
             "coordinates": [72.82815459698692, 18.94324557965778]
         },
         "properties": {
-            "name": "Pizza outlet 2",
+            "name": "Deepesh's Kitchen 2",
             "address": "Shop No 7, Ground Floor, Chemox House, Hospital Lane, Barrack Rd, New Marine Lines, Mumbai, Maharashtra 400020, India",
             "phone": "23 2323 2323"
         }
@@ -30,7 +31,7 @@ const storeList = [
             "coordinates": [72.81416419471911, 18.958588300624903]
         },
         "properties": {
-            "name": "Pizza outlet 3",
+            "name": "Deepesh's Kitchen 3",
             "address": "Mathew Rd, Charni Road East, Opera House, Gamdevi, Mumbai, Maharashtra 400004, India",
             "phone": "23 2323 2323"
         }
@@ -42,7 +43,7 @@ const storeList = [
             "coordinates": [72.82468278992283, 18.99953227821179]
         },
         "properties": {
-            "name": "Pizza outlet 4",
+            "name": "Deepesh's Kitchen 4",
             "address": "Unit NO. 6D, Block-17, Phoenix Mills Compound, 462, Senapati Bapat Marg, Lower Parel, Mumbai, Maharashtra 400013, India",
             "phone": "23 2323 2323"
         }
@@ -54,7 +55,7 @@ const storeList = [
             "coordinates": [72.87461342204949, 19.079330659542418]
         },
         "properties": {
-            "name": "Pizza outlet 5",
+            "name": "Deepesh's Kitchen 5",
             "address": "Shop No 2, Ground floor Kanakia Zillion, LBS Marg, Kurla West, Mumbai, Maharashtra 400070, India",
             "phone": "23 2323 2323"
         }
@@ -66,7 +67,7 @@ const storeList = [
             "coordinates": [72.87289714156712, 19.229326479731387]
         },
         "properties": {
-            "name": "Pizza outlet 6",
+            "name": "Deepesh's Kitchen 6",
             "address": "No. 1-5, Building, Shop, No. 35, Evershine Millennium Paradise, Thakur Village, Kandivali East, Mumbai, Maharashtra 400101, India",
             "phone": "23 2323 2323"
         }
@@ -78,7 +79,7 @@ const storeList = [
             "coordinates": [77.20889596191188, 28.68797557618975]
         },
         "properties": {
-            "name": "Pizza outlet 7",
+            "name": "Deepesh's Kitchen 7",
             "address": "Ground Floor,Shop No. A,Municipal No. 26A & 27A-UA Jawahar Nagar,Kamla Nagar, near Malka Ganj, Chowk, Delhi 110007, India",
             "phone": "23 2323 2323"
         }
@@ -90,7 +91,7 @@ const storeList = [
             "coordinates": [77.11585549399085, 28.741270652258336]
         },
         "properties": {
-            "name": "Pizza outlet 8",
+            "name": "Deepesh's Kitchen 8",
             "address": "SHOP NO. 10-11, COMMUNITY CENTRE, Garg Mall, Pocket 4, Sector 11, Rohini, New Delhi, Delhi 110020, India",
             "phone": "23 2323 2323"
         }
@@ -102,7 +103,7 @@ const storeList = [
             "coordinates": [77.30262307535264, 28.635858680378387]
         },
         "properties": {
-            "name": "Pizza outlet 9",
+            "name": "Deepesh's Kitchen 9",
             "address": "Plot No. 7,LSC, Pankaj Plaza, near Prince Apartments, I.P.Extension, Patparganj, New Delhi, Delhi 110092, India",
             "phone": "23 2323 2323"
         }
@@ -114,7 +115,7 @@ const storeList = [
             "coordinates": [77.27116736919079, 28.539914829877652]
         },
         "properties": {
-            "name": "Pizza outlet 10",
+            "name": "Deepesh's Kitchen 10",
             "address": "Plot No.16, Ground Floor, Community Center Rd, Pocket A, Okhla Phase I, Okhla Industrial Area, New Delhi, Delhi 110020, India",
             "phone": "23 2323 2323"
         }
@@ -126,7 +127,7 @@ const storeList = [
             "coordinates": [88.37799509128703, 22.632535906298756]
         },
         "properties": {
-            "name": "Pizza outlet 11",
+            "name": "Deepesh's Kitchen 11",
             "address": "Ground Floor, Shop no. 3 & 4, Dist, Chinar Park, Noapara, Sukanta Pally, Newtown, Kolkata, West Bengal 700157, India",
             "phone": "23 2323 2323"
         }
@@ -138,7 +139,7 @@ const storeList = [
             "coordinates": [88.35190256153795, 22.56692525562484]
         },
         "properties": {
-            "name": "Pizza outlet 12",
+            "name": "Deepesh's Kitchen 12",
             "address": "3A, Humayun Place Near New Empire Cinema, Esplanade, New Market Area, Dharmatala, Kolkata, West Bengal 700087, India",
             "phone": "23 2323 2323"
         }
@@ -150,7 +151,7 @@ const storeList = [
             "coordinates": [88.38795145132286, 22.70413303586267]
         },
         "properties": {
-            "name": "Pizza outlet 13",
+            "name": "Deepesh's Kitchen 13",
             "address": "Municipality, 352/1889, Dakshminayan Barasat Road Under Panhihati, Sodepur, Kolkata, West Bengal 700110, India",
             "phone": "23 2323 2323"
         }
@@ -162,7 +163,7 @@ const storeList = [
             "coordinates": [88.3251233867392, 22.520947492726815]
         },
         "properties": {
-            "name": "Pizza outlet 14",
+            "name": "Deepesh's Kitchen 14",
             "address": "Plot No. 99A, Block F, Nalini Ranjan Ave, New Alipore, Kolkata, West Bengal 700053, India",
             "phone": "23 2323 2323"
         }
@@ -174,7 +175,7 @@ const storeList = [
             "coordinates": [72.52382422967368, 23.080883179752515]
         },
         "properties": {
-            "name": "Pizza outlet 15",
+            "name": "Deepesh's Kitchen 15",
             "address": "Plot No. 50 & 51, Survey No. 301/2, 313/1, 304/2, Shop No. G - 7 & 8 Ground Floor, Opp. Gujarat High Court, S. G. Road Town Planning Scheme No. 28, Ahmedabad, Gujarat 380061, India",
             "phone": "23 2323 2323"
         }
@@ -186,7 +187,7 @@ const storeList = [
             "coordinates": [72.58939887680621, 23.055613560773505]
         },
         "properties": {
-            "name": "Pizza outlet 16",
+            "name": "Deepesh's Kitchen 16",
             "address": "Neelgagan Plaza, 1st And 2nd Floor Survey Number 2259 Commissioner Office, Dr Ambedkar Rd, Opposite Police, Jain Colony, Shahibag, Ahmedabad, Gujarat 380004, India",
             "phone": "23 2323 2323"
         }
@@ -198,7 +199,7 @@ const storeList = [
             "coordinates": [72.47507239777408, 23.033498752157918]
         },
         "properties": {
-            "name": "Pizza outlet 17",
+            "name": "Deepesh's Kitchen 17",
             "address": "A1 Amrapali Axiom, Sardar Patel Ring Rd, Ambli, Ahmedabad, Gujarat 380058, India",
             "phone": "23 2323 2323"
         }
@@ -210,7 +211,7 @@ const storeList = [
             "coordinates": [72.66939307985272, 23.05340224332604]
         },
         "properties": {
-            "name": "Pizza outlet 18",
+            "name": "Deepesh's Kitchen 18",
             "address": "Shop Number A/03 & A/04, Ground Floor, Gujarat 382350, India",
             "phone": "23 2323 2323"
         }
@@ -222,7 +223,7 @@ const storeList = [
             "coordinates": [73.87720724616587, 18.57728849915506]
         },
         "properties": {
-            "name": "Pizza outlet 19",
+            "name": "Deepesh's Kitchen 19",
             "address": "Shop no 31/A, Ground floor, Shoppers Orbit, Next Mahalaxsmi Complex Near Visharantwadi Police Chowki, Dhanori Rd, Pune, Maharashtra 411015, India",
             "phone": "23 2323 2323"
         }
@@ -234,7 +235,7 @@ const storeList = [
             "coordinates": [73.93213888774288, 18.495259471467133]
         },
         "properties": {
-            "name": "Pizza outlet 20",
+            "name": "Deepesh's Kitchen 20",
             "address": "Shop No:4,5,14,15,16 & 17 Fortune Plaza, Hadapsar, Pune, Maharashtra 411028, India",
             "phone": "23 2323 2323"
         }
@@ -246,7 +247,7 @@ const storeList = [
             "coordinates": [73.82364889562831, 18.48256098719139]
         },
         "properties": {
-            "name": "Pizza outlet 21",
+            "name": "Deepesh's Kitchen 21",
             "address": "Plot No. 7, Dhanlaxmi Society, Opp. Jagtap Hospital, Pune, Maharashtra 411051, India",
             "phone": "23 2323 2323"
         }
@@ -258,7 +259,7 @@ const storeList = [
             "coordinates": [73.84321829294011, 18.52228131664155]
         },
         "properties": {
-            "name": "Pizza outlet 22",
+            "name": "Deepesh's Kitchen 22",
             "address": "Bus Stop, Shop No. 6 & 7, opposite Deccan Gymkhana, Pune, Maharashtra 411004, India",
             "phone": "23 2323 2323"
         }
@@ -270,7 +271,7 @@ const storeList = [
             "coordinates": [73.79308003312877, 18.496413470622766]
         },
         "properties": {
-            "name": "Pizza outlet 23",
+            "name": "Deepesh's Kitchen 23",
             "address": "Cipla Foundation, Survey No. 116/1, Shop No. 2, Opposite, Warje, Pune, Maharashtra 411058, India",
             "phone": "23 2323 2323"
         }
@@ -282,7 +283,7 @@ const storeList = [
             "coordinates": [78.68705181099095, 10.799619385080312]
         },
         "properties": {
-            "name": "Pizza outlet 24",
+            "name": "Deepesh's Kitchen 24",
             "address": "Dukes Complex, 120, Bharathiar Salai, Melapudur, Cantonment, Tiruchirappalli, Tamil Nadu 620001, India",
             "phone": "23 2323 2323"
         }
@@ -294,7 +295,7 @@ const storeList = [
             "coordinates": [78.68405585878058, 10.834835813062087]
         },
         "properties": {
-            "name": "Pizza outlet 25",
+            "name": "Deepesh's Kitchen 25",
             "address": "Vignesh Plaza, A-6, Ground Floor, Thillai Nagar Main Rd, Tiruchirappalli, Tamil Nadu 620018, India",
             "phone": "23 2323 2323"
         }
@@ -306,7 +307,7 @@ const storeList = [
             "coordinates": [74.63322891528135, 26.582440343492785]
         },
         "properties": {
-            "name": "Pizza outlet 26",
+            "name": "Deepesh's Kitchen 26",
             "address": "Shop No. G5, G6 and G7, Ground Floor, Jaipur Rd, Hathi Bhata, Ajmer, Rajasthan 305001, India",
             "phone": "23 2323 2323"
         }
@@ -318,7 +319,7 @@ const storeList = [
             "coordinates": [75.12212052531665, 27.779609685150188]
         },
         "properties": {
-            "name": "Pizza outlet 27",
+            "name": "Deepesh's Kitchen 27",
             "address": "Domino'S Pizza Ground Floor Shop No 2 Shiv Mandir cinema, Fatehpur Rd, Sikar, Rajasthan 332001, India",
             "phone": "23 2323 2323"
         }
@@ -330,7 +331,7 @@ const storeList = [
             "coordinates": [76.77930464002718, 28.378539373887044]
         },
         "properties": {
-            "name": "Pizza outlet 28",
+            "name": "Deepesh's Kitchen 28",
             "address": "Khasra No. 489 & 490, V Square Mall, Alwar Bypass Road, Sector-15, Bhiwadi, Rajasthan 301019, India",
             "phone": "23 2323 2323"
         }
@@ -342,7 +343,7 @@ const storeList = [
             "coordinates": [75.85537784187994, 25.13798803924888]
         },
         "properties": {
-            "name": "Pizza outlet 29",
+            "name": "Deepesh's Kitchen 29",
             "address": "A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
             "phone": "23 2323 2323"
         }
